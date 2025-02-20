ce6468422cdc81023d220577b999be2c7900dad3
ONE-18 <juangranda2002@gamil.com>
2025-02-20 13:45:13 +0100

    Add dotenv support and update server configuration

--- server.ts
+++ server.ts
@@ -3,26 +3,32 @@ import Router from "@koa/router";
 import { exec } from "child_process";
 import fs from "fs";
 import util from "util";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = new Koa();
 const router = new Router();
 const execPromise = util.promisify(exec);
+
+const PORT = process.env.PORT || 5000;
+const COMMANDS_PATH = process.env.COMMANDS_PATH || "commands.json";
 
 // Carga el diccionario desde un archivo JSON
 function loadCommands(): Record<string, string> {
     try {
         // Intentar copiar el contenido del enlace simbólico antes de leer el archivo real
         try {
-            const linkContent = fs.readFileSync("commands.json.lnk", "utf8");
-            fs.writeFileSync("commands.json", linkContent);
+            const linkContent = fs.readFileSync(`${COMMANDS_PATH}.lnk`, "utf8");
+            fs.writeFileSync(COMMANDS_PATH, linkContent);
         } catch (error) {
-            console.error(`Error cargando commands.json.lnk: ${error}`);
+            console.error(`Error cargando ${COMMANDS_PATH}.lnk: ${error}`);
         }
 
-        const data = fs.readFileSync("commands.json", "utf8");
+        const data = fs.readFileSync(COMMANDS_PATH, "utf8");
         return JSON.parse(data);
     } catch (error) {
-        console.error(`Error cargando commands.json: ${error}`);
+        console.error(`Error cargando ${COMMANDS_PATH}: ${error}`);
         return {};
     }
 }
@@ -68,7 +74,6 @@ router.get("/:key", async (ctx) => {
 
 app.use(router.routes()).use(router.allowedMethods());
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Servidor Koa corriendo en http://0.0.0.0:${PORT}`);
 });
